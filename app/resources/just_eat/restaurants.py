a38d582380b73c5d47ea7ea432b0798ca6a5839a
Florian Pont <florian1.pont@free.fr>
2021-03-25 11:55:35 +0100

    feat(restaurant):
    get restaurant by id

--- app/resources/just_eat/restaurants.py
+++ app/resources/just_eat/restaurants.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse, abort
 import requests
+from app.resources.just_eat.utils import *
 
 
 class RestaurantsByLatLong(Resource):
@@ -7,13 +8,23 @@ class RestaurantsByLatLong(Resource):
         body_parser = reqparse.RequestParser()
         body_parser.add_argument('lat', type=str, required=True, help="Missing the lat of the user")
         body_parser.add_argument('lon', type=str, required=True, help="Missing the lon of the user")
-        args = body_parser.parse_args(
-            strict=True)  # Accepted only if these two parameters are strictly declared in body else raise exception
+        # Accepted only if these two parameters are strictly declared in body else raise exception
+        args = body_parser.parse_args(strict=True)
+
         try:
             lat = args['lat']
             lon = args['lon']
-            restaurants = requests.get('https://uk.api.just-eat.io/restaurants/bylatlong',
-                                       params={'latitude': lat, 'longitude': lon})
-            return restaurants.json()
+
+            country_code = get_country_code_from_lat_lon(lat, lon)
+            tenant = get_tenant_from_country_code(country_code)
+
+            url = 'https://{0}.api.just-eat.io/restaurants/bylatlong'.format(tenant)
+
+            restaurants = requests.get(url,
+                                       params={'latitude': float(lat), 'longitude': float(lon)},
+                                       headers={'Accept-Tenant': country_code})
+
+            return {"status": 200, "message": "OK", "data": restaurants.json()}
         except Exception as e:
+            print(e)
             abort(400, status=400, message="Bad Request", data="")
---
+++ app/resources/just_eat/utils.py
@@ -0,0 +1,21 @@
+from geopy.geocoders import Nominatim
+
+
+def get_country_code_from_lat_lon(lat, lon):
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.reverse(lat + "," + lon)
+    country_code = location.raw['address'].get('country_code')
+    if country_code == 'gb':
+        country_code = 'uk'
+
+    return country_code
+
+
+def get_tenant_from_country_code(country_code):
+    if country_code == 'uk':
+        tenant = country_code
+    elif country_code == 'au' or country_code == 'nz':
+        tenant = 'aus'
+    else:
+        tenant = 'i18n'
+    return tenant
