c860d0016858e2aed0261bd07914bb69e0cf78fa
Florian Pont <florian1.pont@free.fr>
2021-03-27 17:06:24 +0100

    feat(restaurants):
    add restaurants with a searchTerm

--- app/resources/restaurants.py
+++ app/resources/restaurants.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse, abort
-from app.resources.just_eat.restaurants import get_just_eat_restaurants
+from app.resources.just_eat.restaurants import get_just_eat_restaurants, get_just_eat_restaurant_search
 from app.resources.deliveroo.restaurants import get_deliveroo_restaurants
 
 
@@ -20,7 +20,32 @@ class RestaurantsByLatLong(Resource):
             restaurants.extend(get_just_eat_restaurants(lat, lon))
             restaurants.extend(get_deliveroo_restaurants(lat, lon))
 
-            return restaurants
+            return {"status": 200, "message": "OK", "data": restaurants}
+
+        except Exception as e:
+            print(e)
+            abort(400, status=400, message="Bad Request", data=e.__str__())
+
+
+class RestaurantsByLatLongBySearch(Resource):
+    def post(self):
+        body_parser = reqparse.RequestParser()
+        body_parser.add_argument('lat', type=str, required=True, help="Missing the latitude")
+        body_parser.add_argument('lon', type=str, required=True, help="Missing the longitude")
+        body_parser.add_argument('searchTerm', type=str, required=True, help="Missing the searchTerm")
+        # Accepted only if these two parameters are strictly declared in body else raise exception
+        args = body_parser.parse_args(strict=True)
+
+        try:
+            lat = args['lat']
+            lon = args['lon']
+            search_term = args['searchTerm']
+
+            restaurants = []
+
+            restaurants.extend(get_just_eat_restaurant_search(lat, lon, search_term))
+
+            return {"status": 200, "message": "OK", "data": restaurants}
 
         except Exception as e:
             print(e)
--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,11 @@ from app import api
 
 from app.resources.hello import HelloWorld
 from app.resources.deliveroo.restaurant import restaurantByIDDeliverooResource
-from app.resources.restaurants import RestaurantsByLatLong
+from app.resources.restaurants import *
 from app.resources.just_eat.restaurant import RestaurantByID
 
 api.add_resource(HelloWorld, "/")
 api.add_resource(restaurantByIDDeliverooResource, '/restaurantDeliveroo/<int:id_restaurant>')
 api.add_resource(RestaurantsByLatLong, "/restaurants")
+api.add_resource(RestaurantsByLatLongBySearch, "/restaurants/search")
 api.add_resource(RestaurantByID, "/restaurant/<int:restaurant_id>")
