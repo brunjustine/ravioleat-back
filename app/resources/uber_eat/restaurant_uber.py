e8e574243d8ee294dc3bcce7ea152676a0c21496
justine brun <just.brun@hotmail.fr>
2021-03-29 11:13:56 +0200

    formatted response ok

--- app/resources/uber_eat/restaurant_uber.py
+++ app/resources/uber_eat/restaurant_uber.py
@@ -3,7 +3,9 @@ from flask_restful import Resource, reqparse, abort
 from typing import Dict, List, Any
 import requests
 
-RESTAURANTS = []
+from app.services.restaurantService import RESTAURANT
+
+UBER_RESTAURANTS = []
 
 class RestaurantUberCategoryResource(Resource):
     def post(self) -> Dict[str, Any]:
@@ -124,10 +126,12 @@ class RestaurantUberResource(Resource):
             abort(400)
         categories = call_category(params)
         #categories = ['pizza']
-        del RESTAURANTS[:]
-        RESTAURANTS.append(get_all_restaurants(params, categories))
-        return RESTAURANTS
-    
+        del UBER_RESTAURANTS[:]
+        UBER_RESTAURANTS.append(get_all_restaurants(params, categories))
+        liste_restaurants = initResto()
+        return liste_restaurants
+        #return UBER_RESTAURANTS
+
 class RestaurantUberByIdResource(Resource):
     def get(self, restaurant_id: str) -> Dict[str,Any]:
         """
@@ -149,6 +153,7 @@ class RestaurantUberByIdResource(Resource):
         """
         abort_if_restaurant_empty()
         return get_restaurant_by_id(restaurant_id)   
+        #return initResto().json()
 
 def call_category(params: Dict[str, Any]):
     url = "http://0.0.0.0:5000/restaurants/uber/categories"
@@ -166,16 +171,10 @@ def call_category(params: Dict[str, Any]):
 #def get_categories(categories: Dict[str, Any]):
 #    return sum(list(map(lambda grid: list(map(lambda cat : cat['title'], grid['gridItems'])) , categories['suggestedSections'])),[])
 
-
+#get top categories
 def get_categories(categories: Dict[str, Any]):
     top_categories = next((x for x in categories['suggestedSections'] if x['title'] == "Top categories"), None)
     return list(map(lambda category: category['title'] , top_categories['gridItems']))
-
-#def get_all_restaurants(params:Dict[str, Any] , categories : Dict[str, Any]):
-#    restaurants=[]
-#    for category in categories:
-#        restaurants.append(call_search(params,category))
-#    return restaurants
 
 def get_all_restaurants(params:Dict[str, Any] , categories : Dict[str, Any]):
     categories_regex = ""
@@ -195,12 +194,61 @@ def call_search(params : Dict[str, Any], category:str):
     return requests.post(url, json=data, headers=headers).json()
 
 def abort_if_restaurant_empty():
-    if not RESTAURANTS:
+    if not UBER_RESTAURANTS:
         abort(404,message ='Error : No restaurants found')
 
 
 def get_restaurant_by_id(restaurant_id : str):
-    restaurant_details = RESTAURANTS[0]['feed']['feedItems']
-    map_details = RESTAURANTS[0]["feed"]["storesMap"]
+    restaurant_details = UBER_RESTAURANTS[0]['feed']['feedItems']
+    map_details = UBER_RESTAURANTS[0]["feed"]["storesMap"]
     return list(filter(lambda e: e['uuid']==restaurant_id, restaurant_details))
     #return map_details
+
+def initResto():
+    liste_restaurants = []
+    for resto in UBER_RESTAURANTS[0]['feed']['feedItems']:
+        restaurant_model = RESTAURANT.copy()
+        uuid = resto['uuid']
+        attributs = UBER_RESTAURANTS[0]['feed']['storesMap'][uuid]
+        restaurant_model.__setitem__("Api","uber_eat")
+        restaurant_model.__setitem__("Id",uuid)
+        restaurant_model.__setitem__("Name",attributs["title"])
+        restaurant_model.__setitem__("UniqueName",None)
+        restaurant_model.__setitem__("Address",{
+            "City": attributs["location"]["address"]["city"],
+            "FirstLine": attributs["location"]["address"]["address1"],
+            "Postcode": attributs["location"]["address"]["postalCode"],
+            "Latitude": attributs["location"]["latitude"],
+            "Longitude": attributs["location"]["longitude"]
+        })
+
+        restaurant_model.__setitem__("Rating",{
+            "Count": attributs.get("rawRatingStats", {}).get('ratingCount'),
+            "StarRating": attributs.get("rawRatingStats", {}).get('storeRatingScore')
+        })
+       
+        restaurant_model.__setitem__("Description", None)
+        #todo check if url == url uber_eat+uuid
+        restaurant_model.__setitem__("Url",None)
+        restaurant_model.__setitem__("LogoUrl",attributs["heroImageUrl"])
+        
+        available = resto["payload"]["storePayload"]["stateMapDisplayInfo"]["available"]["subtitle"]["text"].split(" min")
+        restaurant_model.__setitem__("DeliveryEtaMinutes",{
+            "RangeLower": available[0].split("–")[0],
+            "RangeUpper": available[0].split("–")[1]
+        })
+        restaurant_model.__setitem__("IsOpenNow",attributs["isOrderable"])
+        restaurant_model.__setitem__("DeliveryCost", attributs.get("fareInfo", {}).get('serviceFee'))
+        restaurant_model.__setitem__("Offers",None)
+        
+        restaurant_model.__setitem__("CuisineTypes", [
+            {
+            "Id": category["uuid"],
+            "Name": category["name"],
+            "SeoName": category["keyName"]
+            }
+            for category in attributs["categories"]
+        ] if attributs["categories"] is not None else [])
+        restaurant_model.__setitem__("PriceCategory", len(attributs.get("priceBucket","")))
+        liste_restaurants.append(restaurant_model)
+    return liste_restaurants
