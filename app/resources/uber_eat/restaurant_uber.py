dc2099f52cc852a80aded825dbca4d582c0c7c83
justine brun <just.brun@hotmail.fr>
2021-03-29 14:34:08 +0200

    search+all restaurants ok+check type ==store in init fn

--- app/resources/uber_eat/restaurant_uber.py
+++ app/resources/uber_eat/restaurant_uber.py
@@ -115,6 +115,8 @@ class RestaurantUberResource(Resource):
             'longitude', type=str, required=True, help="Missing the longitude")
         body_parser.add_argument(
             'formattedAddress', type=str, required=True, help="Missing the address ")
+        body_parser.add_argument(
+            'userQuery', type=str, required=True, help="Missing the user query ")
         args = body_parser.parse_args(strict=True)
 
         try:  
@@ -122,12 +124,12 @@ class RestaurantUberResource(Resource):
             longitude = args['longitude']
             formatted_address = args['formattedAddress']
             params = {"latitude":float(latitude), "longitude" :float(longitude) , "formatted_address": formatted_address}  
+            params['user_query'] = args['userQuery'] if args['userQuery'] != "" else get_formatted_categories(params)
         except:
             abort(400)
-        categories = call_category(params)
         #categories = ['pizza']
         del UBER_RESTAURANTS[:]
-        UBER_RESTAURANTS.append(get_all_restaurants(params, categories))
+        UBER_RESTAURANTS.append(call_search(params))
         liste_restaurants = initResto()
         return liste_restaurants
         #return UBER_RESTAURANTS
@@ -176,19 +178,20 @@ def get_categories(categories: Dict[str, Any]):
     top_categories = next((x for x in categories['suggestedSections'] if x['title'] == "Top categories"), None)
     return list(map(lambda category: category['title'] , top_categories['gridItems']))
 
-def get_all_restaurants(params:Dict[str, Any] , categories : Dict[str, Any]):
+def get_formatted_categories(params: Dict[str, Any]):
+    categories = call_category(params)
     categories_regex = ""
     for category in categories:
         categories_regex += category+"|"
-    return call_search(params, categories_regex)
+    return categories_regex
 
-def call_search(params : Dict[str, Any], category:str):
+def call_search(params : Dict[str, Any]):
     url = "http://0.0.0.0:5000/restaurants/uber/search"
     data = {
             "formattedAddress": params['formatted_address'],
             "latitude": params['latitude'],                                                                                        
             "longitude": params['longitude'],
-            "userQuery": category
+            "userQuery": params['user_query']
     }
     headers = {'Content-Type': 'application/json'}
     return requests.post(url, json=data, headers=headers).json()
@@ -207,48 +210,51 @@ def get_restaurant_by_id(restaurant_id : str):
 def initResto():
     liste_restaurants = []
     for resto in UBER_RESTAURANTS[0]['feed']['feedItems']:
-        restaurant_model = RESTAURANT.copy()
-        uuid = resto['uuid']
-        attributs = UBER_RESTAURANTS[0]['feed']['storesMap'][uuid]
-        restaurant_model.__setitem__("Api","uber_eat")
-        restaurant_model.__setitem__("Id",uuid)
-        restaurant_model.__setitem__("Name",attributs["title"])
-        restaurant_model.__setitem__("UniqueName",None)
-        restaurant_model.__setitem__("Address",{
-            "City": attributs["location"]["address"]["city"],
-            "FirstLine": attributs["location"]["address"]["address1"],
-            "Postcode": attributs["location"]["address"]["postalCode"],
-            "Latitude": attributs["location"]["latitude"],
-            "Longitude": attributs["location"]["longitude"]
-        })
+        if resto['type'] == 'STORE':
+            restaurant_model = RESTAURANT.copy()
+            uuid = resto['uuid']
+            attributs = UBER_RESTAURANTS[0]['feed']['storesMap'][uuid]
+            restaurant_model.__setitem__("Api","uber_eat")
+            restaurant_model.__setitem__("Id",uuid)
+            restaurant_model.__setitem__("Name",attributs["title"])
+            restaurant_model.__setitem__("UniqueName",None)
+            restaurant_model.__setitem__("Address",{
+                "City": attributs["location"]["address"]["city"],
+                "FirstLine": attributs["location"]["address"]["address1"],
+                "Postcode": attributs["location"]["address"]["postalCode"],
+                "Latitude": attributs["location"]["latitude"],
+                "Longitude": attributs["location"]["longitude"]
+            })
 
-        restaurant_model.__setitem__("Rating",{
-            "Count": attributs.get("rawRatingStats", {}).get('ratingCount'),
-            "StarRating": attributs.get("rawRatingStats", {}).get('storeRatingScore')
-        })
-       
-        restaurant_model.__setitem__("Description", None)
-        #todo check if url == url uber_eat+uuid
-        restaurant_model.__setitem__("Url",None)
-        restaurant_model.__setitem__("LogoUrl",attributs["heroImageUrl"])
+            restaurant_model.__setitem__("Rating",{
+                "Count": attributs.get("rawRatingStats", {}).get('ratingCount'),
+                "StarRating": attributs.get("rawRatingStats", {}).get('storeRatingScore')
+            })
         
-        available = resto["payload"]["storePayload"]["stateMapDisplayInfo"]["available"]["subtitle"]["text"].split(" min")
-        restaurant_model.__setitem__("DeliveryEtaMinutes",{
-            "RangeLower": available[0].split("–")[0],
-            "RangeUpper": available[0].split("–")[1]
-        })
-        restaurant_model.__setitem__("IsOpenNow",attributs["isOrderable"])
-        restaurant_model.__setitem__("DeliveryCost", attributs.get("fareInfo", {}).get('serviceFee'))
-        restaurant_model.__setitem__("Offers",None)
-        
-        restaurant_model.__setitem__("CuisineTypes", [
-            {
-            "Id": category["uuid"],
-            "Name": category["name"],
-            "SeoName": category["keyName"]
-            }
-            for category in attributs["categories"]
-        ] if attributs["categories"] is not None else [])
-        restaurant_model.__setitem__("PriceCategory", len(attributs.get("priceBucket","")))
-        liste_restaurants.append(restaurant_model)
+            restaurant_model.__setitem__("Description", None)
+            #todo check if url == url uber_eat+uuid
+            restaurant_model.__setitem__("Url",None)
+            restaurant_model.__setitem__("LogoUrl",attributs.get("heroImageUrl",""))
+            
+            available = resto["payload"]["storePayload"]["stateMapDisplayInfo"]["available"]["subtitle"]["text"].split(" min")
+            restaurant_model.__setitem__("DeliveryEtaMinutes",{
+                "RangeLower": available[0].split("–")[0],
+                "RangeUpper": available[0].split("–")[1]
+            })
+            restaurant_model.__setitem__("IsOpenNow",attributs["isOrderable"])
+            restaurant_model.__setitem__("DeliveryCost", attributs.get("fareInfo", {}).get('serviceFee'))
+            restaurant_model.__setitem__("Offers",None)
+            
+            restaurant_model.__setitem__("CuisineTypes", [
+                {
+                "Id": category["uuid"],
+                "Name": category["name"],
+                "SeoName": category["keyName"]
+                }
+                for category in attributs["categories"]
+            ] if attributs["categories"] is not None else [])
+            restaurant_model.__setitem__("PriceCategory", len(attributs.get("priceBucket","")))
+            liste_restaurants.append(restaurant_model)
     return liste_restaurants
+
+    
---
+++ app/services/restaurantsWithMenuService.py
@@ -0,0 +1,48 @@
+RESTAURANT_WITH_MENU = {
+    "Api": str,
+    "Id": str,
+    "Name": str,
+    "UniqueName": str,
+    "Address": {
+       "City": str,
+       "FirstLine": str,
+       "Postcode": str,
+       "Latitude": float,
+       "Longitude": float
+    },
+    "Rating": {
+       "Count": str,
+       "StarRating": float
+    },
+    "Description": str,
+    "Url": str,
+    "LogoUrl": str,
+    "DeliveryEtaMinutes": {
+       "RangeLower": int,
+       "RangeUpper": int
+    },
+    "IsOpenNow": bool,
+    "DeliveryCost": float,
+    "Offers": [
+        {
+            "Description": str,
+            "OfferId": int
+        }
+    ],
+    "CuisineTypes": [
+       {
+           "Id": str,
+           "Name": str,
+           "SeoName": str
+       }
+    ],
+    "PriceCategory": int,
+    "Menus": [
+        {
+            "Id": int,
+            "Name": str,
+            "Description": str,
+            "Price": float,
+        }
+    ],
+}
