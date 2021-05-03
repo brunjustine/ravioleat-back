c0692991400b992ef63f30f78ab984512d91cba4
Florian Pont <florian1.pont@free.fr>
2021-05-03 10:44:35 +0200

    refactor(uber_EatCategories): refacto

--- app/resources/uber_eat/categories.py
+++ app/resources/uber_eat/categories.py
@@ -1,9 +1,8 @@
-from flask import request, jsonify, json
-from flask_restful import Resource, reqparse, abort
 from typing import Dict, List, Any
 import requests
 
-def call_category(params: Dict[str, Any]) -> Dict[str, Any]:
+
+def call_category(params: Dict[str, Any]) -> List:
     """
     Return all category restaurants in Uber eat API
     ---
@@ -18,35 +17,32 @@ def call_category(params: Dict[str, Any]) -> Dict[str, Any]:
                 "supportedTypes": ["grid"],
                 "targetLocation": {
                     "address": {
-                    "eaterFormattedAddress": params['formatted_address']
+                        "eaterFormattedAddress": params['formatted_address']
                     },
                     "latitude": float(params['latitude']),                                                                                        
                     "longitude": float(params['longitude'])
-            }
+                }
     }
     headers = {'Content-Type': 'application/json'}
     restaurants = requests.post(url, json=data, headers=headers)
     categories = get_categories(restaurants.json())
     return categories
 
-#get all categories
-#def get_categories(categories: Dict[str, Any]):
-#    return sum(list(map(lambda grid: list(map(lambda cat : cat['title'], grid['gridItems'])) , categories['suggestedSections'])),[])
 
-#get top categories
 def get_categories(categories: Dict[str, Any]):
     if categories['suggestedSections'] is not None:
         top_categories = next((x for x in categories['suggestedSections'] if x['title'] == "Top categories"), None)
-        return list(map(lambda category: category['title'] , top_categories['gridItems']))
-    else :
+        return list(map(lambda category: category['title'], top_categories['gridItems']))
+    else:
         return []
+
 
 def get_formatted_categories(params: Dict[str, Any]):
     categories = call_category(params)
-    if len(categories) > 0 :
+    if len(categories) > 0:
         categories_regex = "" 
         for category in categories:
             categories_regex += category+"|"
-    else :
+    else:
         categories_regex = "" 
     return categories_regex
