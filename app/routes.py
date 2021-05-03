2bc3579f7419c56f5877951b3ceed1f9dd4ebba7
Florian Pont <florian1.pont@free.fr>
2021-05-03 10:59:12 +0200

    refactor(routes):

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,3 @@ from app.resources.restaurant import RestaurantById
 api.add_resource(RestaurantsByLatLong, "/restaurants")
 api.add_resource(RestaurantsByLatLongBySearch, "/restaurants/search")
 api.add_resource(RestaurantById, "/restaurant/<restaurant_id>")
-
